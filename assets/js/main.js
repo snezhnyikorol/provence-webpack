27f0fb1287fc72bb43ab095da3704eaa9757958b
Aliaksei Raketski <raketskij@gmail.com>
2019-07-30 20:07:52 +0300

    committt

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@ let menuState = false;
 
 var $root = $('html, body');
 
+$( document ).ready(function() {
+  move();
+});
+
+
 $('a[href^="#"]').click(function () {
     $root.animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
@@ -38,111 +43,111 @@ $('.map_item').click(function () {
   }
 })
 
+function move() {
+  pillow1 = $('#pillow1').position();
+  pillow2 = $('#pillow2').position();
+  p1 = $('#p1').position();
+  p2 = $('#p2').position();
+  feather1 = $('#feather1').position();
+  feather2 = $('#feather2').position();
+  feather3 = $('#feather3').position();
+  feather4 = $('#feather4').position();
+  feather5 = $('#feather5').position();
+  feather6 = $('#feather6').position();
+  feather7 = $('#feather7').position();
+  feather8 = $('#feather8').position();
+  feather9 = $('#feather9').position();
+  feather10 = $('#feather10').position();
+  feather11 = $('#feather11').position();
+  feather12 = $('#feather12').position();
 
-pillow1 = $('#pillow1').position();
-pillow2 = $('#pillow2').position();
-p1 = $('#p1').position();
-p2 = $('#p2').position();
-feather1 = $('#feather1').position();
-feather2 = $('#feather2').position();
-feather3 = $('#feather3').position();
-feather4 = $('#feather4').position();
-feather5 = $('#feather5').position();
-feather6 = $('#feather6').position();
-feather7 = $('#feather7').position();
-feather8 = $('#feather8').position();
-feather9 = $('#feather9').position();
-feather10 = $('#feather10').position();
-feather11 = $('#feather11').position();
-feather12 = $('#feather12').position();
 
 
+  $(document).on('mousemove', function(e) {
 
-$(document).on('mousemove', function(e) {
-
-    $('#pillow1').css({
-        left: e.pageX / 50 + pillow1.left,
-        top: -e.pageY / 50 + pillow1.top,
+      $('#pillow1').css({
+          left: e.pageX / 50 + pillow1.left,
+          top: -e.pageY / 50 + pillow1.top,
+          filter: 'blur(' + e.pageX / 800 + 'px)'
+      });
+      $('#pillow2').css({
+          left: -e.pageX / 100 + pillow2.left,
+          top: e.pageY / 100 + pillow2.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });
+      $('#p1').css({
+        left: e.pageX / 50 + p1.left,
+        top: -e.pageY / 50 + p1.top,
         filter: 'blur(' + e.pageX / 800 + 'px)'
-    });
-    $('#pillow2').css({
-        left: -e.pageX / 100 + pillow2.left,
-        top: e.pageY / 100 + pillow2.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });
-    $('#p1').css({
-      left: e.pageX / 50 + p1.left,
-      top: -e.pageY / 50 + p1.top,
-      filter: 'blur(' + e.pageX / 800 + 'px)'
-    });
-    $('#p2').css({
-        left: -e.pageX / 100 + p2.left,
-        top: e.pageY / 100 + p2.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });
-    $('#feather1').css({
-        left: -e.pageX / 100 + feather1.left,
-        top: e.pageY / 100 + feather1.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });
-    $('#feather2').css({
-        left: e.pageX / 100 + feather2.left,
-        top: -e.pageY / 100 + feather2.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });
-    $('#feather3').css({
-      left: e.pageX / 50 + feather3.left,
-      top: -e.pageY / 50 + feather3.top,
-      filter: 'blur(' + e.pageX / 800 + 'px)'
-    });
-    $('#feather4').css({
-        left: -e.pageX / 100 + feather4.left,
-        top: e.pageY / 100 + feather4.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });
-    $('#feather5').css({
-      left: e.pageX / 50 + feather5.left,
-      top: -e.pageY / 50 + feather5.top,
-      filter: 'blur(' + e.pageX / 800 + 'px)'
-    });
-    $('#feather6').css({
-        left: -e.pageX / 100 + feather6.left,
-        top: e.pageY / 100 + feather6.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });
-    $('#feather7').css({
-        left: -e.pageX / 100 + feather7.left,
-        top: e.pageY / 100 + feather7.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });
-    $('#feather8').css({
-        left: e.pageX / 100 + feather8.left,
-        top: -e.pageY / 100 + feather8.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });    
-    $('#feather9').css({
-      left: e.pageX / 50 + feather9.left,
-      top: -e.pageY / 50 + feather9.top,
-      filter: 'blur(' + e.pageX / 800 + 'px)'
-    });
-    $('#feather10').css({
-        left: -e.pageX / 100 + feather10.left,
-        top: e.pageY / 100 + feather10.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });
-    $('#feather11').css({
-      left: e.pageX / 50 + feather11.left,
-      top: -e.pageY / 50 + feather11.top,
-      filter: 'blur(' + e.pageX / 800 + 'px)'
-    });
-    $('#feather12').css({
-        left: -e.pageX / 100 + feather12.left,
-        top: e.pageY / 100 + feather12.top,
-        filter: 'blur(' + e.pageX / 1000 + 'px)'
-    });      
-});
+      });
+      $('#p2').css({
+          left: -e.pageX / 100 + p2.left,
+          top: e.pageY / 100 + p2.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });
+      $('#feather1').css({
+          left: -e.pageX / 100 + feather1.left,
+          top: e.pageY / 100 + feather1.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });
+      $('#feather2').css({
+          left: e.pageX / 100 + feather2.left,
+          top: -e.pageY / 100 + feather2.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });
+      $('#feather3').css({
+        left: e.pageX / 50 + feather3.left,
+        top: -e.pageY / 50 + feather3.top,
+        filter: 'blur(' + e.pageX / 800 + 'px)'
+      });
+      $('#feather4').css({
+          left: -e.pageX / 100 + feather4.left,
+          top: e.pageY / 100 + feather4.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });
+      $('#feather5').css({
+        left: e.pageX / 50 + feather5.left,
+        top: -e.pageY / 50 + feather5.top,
+        filter: 'blur(' + e.pageX / 800 + 'px)'
+      });
+      $('#feather6').css({
+          left: -e.pageX / 100 + feather6.left,
+          top: e.pageY / 100 + feather6.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });
+      $('#feather7').css({
+          left: -e.pageX / 100 + feather7.left,
+          top: e.pageY / 100 + feather7.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });
+      $('#feather8').css({
+          left: e.pageX / 100 + feather8.left,
+          top: -e.pageY / 100 + feather8.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });    
+      $('#feather9').css({
+        left: e.pageX / 50 + feather9.left,
+        top: -e.pageY / 50 + feather9.top,
+        filter: 'blur(' + e.pageX / 800 + 'px)'
+      });
+      $('#feather10').css({
+          left: -e.pageX / 100 + feather10.left,
+          top: e.pageY / 100 + feather10.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });
+      $('#feather11').css({
+        left: e.pageX / 50 + feather11.left,
+        top: -e.pageY / 50 + feather11.top,
+        filter: 'blur(' + e.pageX / 800 + 'px)'
+      });
+      $('#feather12').css({
+          left: -e.pageX / 100 + feather12.left,
+          top: e.pageY / 100 + feather12.top,
+          filter: 'blur(' + e.pageX / 1000 + 'px)'
+      });      
+  });
 
-// $('#price_day-out').mask("9?9 суток" ,{placeholder:" ", autoclear: false})
+}
 
 let count = 1;
 const bill = 3500;
