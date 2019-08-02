729e6d7b4f7164afa8d5962734809451a439b907
Aliaksei Raketski <raketskij@gmail.com>
2019-08-03 01:07:05 +0300

    feat: styles for frame

---
+++ assets/js/frame.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     var head  = document.getElementsByTagName('head')[0];
+//     var link  = document.createElement('link');
+//     link.id   = cssId;
+//     link.rel  = 'stylesheet';
+//     link.type = 'text/css';
+//     link.href = 'assets/css/styleFrame.css';
+//     link.media = 'all';
+//     head.appendChild(link);
+// });
