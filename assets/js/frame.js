59884848868881a4dfc1f10b4f23a7b2bc2802cb
Aliaksei Raketski <raketskij@gmail.com>
2019-08-03 01:59:42 +0300

    feat: remove iframe

--- assets/js/frame.js
+++
@@ -1,8 +0,0 @@
-let lang = window.parent.lang
-// document.onload = function () {
-//     console.log(window.parent.lang)
-// }
-
-if (lang == 'eng') {
-    $("[for=rc-checkin]").html('Arrival&nbsp;')
-}
