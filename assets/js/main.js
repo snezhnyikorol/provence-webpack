3807e3350d4545183e1bfb4aaa5b08e568882131
Aliaksei Raketski <raketskij@gmail.com>
2019-08-04 05:07:59 +0300

    feat: almost final

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,8 @@ $(window).scroll(function(){
   } 
 });
 
+
+let rus = {};
 let menuState = false;
 var lang = 'rus'
 
@@ -28,12 +30,6 @@ $( document ).ready(function() {
       lazyLoad: 'progressive'
     }
   )
-
-  $('.rc-flat__book__btn').on('click', function (event) {
-    $('.rc-modal-title').text('booking')
-    comsole.log('jquery')
-    event.preventDefault()
-  })
   
   $('.lightbox').slickLightbox(
     {
@@ -74,6 +70,7 @@ $('#lang_rus').click(function (event) {
     lang = 'rus'
     $('.rus').toggle()
     $('.eng').toggle()
+    translateWidget('rus')
     $('#ourframe').attr('src', $('#ourframe').attr('src'))
     document.title = 'Студии Прованс'
     $('#price_day-out').attr('placeholder', '1 сутки') 
@@ -322,16 +319,27 @@ function setCaretPosition(elemId, caretPos) {
 
 function translateWidget(to) {
   if (to == 'eng') {
+    rus['checkin'] = $("[for=rc-checkin]").html()
     $("[for=rc-checkin]").html('Сheck in&nbsp;')
+    rus['checkout'] = $("[for=rc-checkout]").html()
     $("[for=rc-checkout]").html('Сheck out&nbsp;')
+    rus['guests'] = $("[for=guests]").html()
     $("[for=guests]").html('Guests&nbsp;')
+    rus['option'] = $('.rc-search_form__item__guests__control option:eq(8)').text();
     $('.rc-search_form__item__guests__control option:eq(8)').text('more than 9');
+    rus['rc-search_form__search_btn'] = $(".rc-search_form__search_btn").text()
     $(".rc-search_form__search_btn").text('Search')
-    $(".rc-text-center").text('Loading...')
+    rus['rc-text-center'] =  $(".rc-text-center").text()
+    $("rc-text-center").text('Loading...')
+    rus['rc-flat__price__cur'] = $(".rc-flat__price__cur").text()
     $(".rc-flat__price__cur").text('RUB per day')
+    rus['rc-flat__more__btn"'] = $(".rc-flat__more__btn").text()
     $(".rc-flat__more__btn").text('More')
+    rus['rc-flat__book__btn'] = $(".rc-flat__book__btn").text()
     $(".rc-flat__book__btn").text('Book')
+    rus['rc-flat__info1'] = $(".rc-flat_list .rc-flat:eq(0) .rc-flat__info").html()
     $(".rc-flat_list .rc-flat:eq(0) .rc-flat__info").html(`<span class="rc-flat__info__adress">St. Petersburg, Socialist 13</span><br><!-- react-text: 70 -->m. Zvenigorodskaya<!-- /react-text --><br><!-- react-text: 72 -->Rooms: <!-- /react-text --><!-- react-text: 73 -->1<!-- /react-text --><br><!-- react-text: 75 -->Sleeps: <!-- /react-text --><!-- react-text: 76 -->2+2<!-- /react-text --><br><span><!-- react-text: 79 -->Floor: <!-- /react-text --><!-- react-text: 80 -->1<!-- /react-text --></span><div class="rc-flat__info__services" style=""><i class="rc-icon rc-icon-washing_machine" title="Washer"></i><i class="rc-icon rc-icon-wi_fi" title="Wi-Fi Internet"></i><i class="rc-icon rc-icon-tv" title="TV"></i><i class="rc-icon rc-icon-kids" title="Can children"></i><i class="rc-icon rc-icon-refrigerator" title="Fridge"></i><i class="rc-icon rc-icon-stove" title="Kitchen stove"></i><i class="rc-icon rc-icon-microwave" title="Microwave oven"></i><i class="rc-icon rc-icon-iron" title="Iron and ironing board"></i><i class="rc-icon rc-icon-waterheater" title="Water heater"></i><i class="rc-icon rc-icon-cabletv" title="Cable TV"></i><i class="rc-icon rc-icon-romantic" title="For romantic meetings"></i></div>`)
+    rus['rc-flat__more__info1'] = $(".rc-flat_list .rc-flat:eq(0) .rc-flat__more__info").html()
     $(".rc-flat_list .rc-flat:eq(0) .rc-flat__more__info").html(`<div class="rc-row" style="margin-bottom: 10px;"
     ><div class="rc-col-sm-12 rc-flat__more__info__services">
     <div class="rc-row rc-flat__more__info__services__row">
@@ -358,7 +366,9 @@ function translateWidget(to) {
     <!-- react-text: 148 -->&nbsp;<!-- /react-text --><!-- react-text: 149 -->For romantic meetings<!-- /react-text --></div></div></div></div><div class="rc-row">
     <div class="rc-col-sm-12 rc-flat__more__info__desc"><p>&nbsp;Center, to Nevsky 15 minutes on foot. Nearby are three metro stations (Vladimirskaya, Dostoevskaya, Zvenigorodskaya), restaurants, shops, cafes, market, shopping centers, oceanarium. Nevsky is 1 km away. Cozy studio apartment in the historic city center. The apartment is completely ready for a comfortable stay - design repair, there are all necessary furniture and appliances, cable TV, Internet. double bed 160 cm, double sofa bed 160 cm, chest of drawers, wardrobe, TV, dining table, chairs, stools, bedding, kitchen utensils, dishes.<br>Cozy, comfortable, reliable.</p>
     </div></div><div class="rc-row"><div class="rc-col-sm-12 rc-flat__more__info__map_wrapper"></div></div>`)
+    rus['rc-flat__info2'] = $(".rc-flat_list .rc-flat:eq(1) .rc-flat__info").html()
     $(".rc-flat_list .rc-flat:eq(1) .rc-flat__info").html(`<span class="rc-flat__info__adress">St. Petersburg, Socialist 13</span><br><!-- react-text: 70 -->m. Zvenigorodskaya<!-- /react-text --><br><!-- react-text: 72 -->Rooms: <!-- /react-text --><!-- react-text: 73 -->1<!-- /react-text --><br><!-- react-text: 75 -->Sleeps: <!-- /react-text --><!-- react-text: 76 -->2+2<!-- /react-text --><br><span><!-- react-text: 79 -->Floor: <!-- /react-text --><!-- react-text: 80 -->1<!-- /react-text --></span><div class="rc-flat__info__services" style=""><i class="rc-icon rc-icon-washing_machine" title="Washer"></i><i class="rc-icon rc-icon-wi_fi" title="Wi-Fi Internet"></i><i class="rc-icon rc-icon-tv" title="TV"></i><i class="rc-icon rc-icon-kids" title="Can children"></i><i class="rc-icon rc-icon-refrigerator" title="Fridge"></i><i class="rc-icon rc-icon-stove" title="Kitchen stove"></i><i class="rc-icon rc-icon-microwave" title="Microwave oven"></i><i class="rc-icon rc-icon-iron" title="Iron and ironing board"></i><i class="rc-icon rc-icon-waterheater" title="Water heater"></i><i class="rc-icon rc-icon-cabletv" title="Cable TV"></i><i class="rc-icon rc-icon-romantic" title="For romantic meetings"></i></div>`)
+    rus['rc-flat__more__info2'] = $(".rc-flat_list .rc-flat:eq(1) .rc-flat__more__info").html()
     $(".rc-flat_list .rc-flat:eq(1) .rc-flat__more__info").html(`<div class="rc-row" style="margin-bottom: 10px;"
     ><div class="rc-col-sm-12 rc-flat__more__info__services">
     <div class="rc-row rc-flat__more__info__services__row">
@@ -388,29 +398,19 @@ function translateWidget(to) {
 
 
     $(".rc-modal-title").text('Booking')
+  } else if (to == 'rus') {
+    $("[for=rc-checkin]").html(rus['checkin'])
+    $("[for=rc-checkout]").html(rus['checkout'])
+    $("[for=guests]").html(rus['guests'] )
+    $('.rc-search_form__item__guests__control option:eq(8)').text(rus['option']);
+    $(".rc-search_form__search_btn").text(rus['rc-search_form__search_btn'])
+    $("rc-text-center").text(rus['rc-text-center'] )
+    $(".rc-flat__price__cur").text(rus['rc-flat__price__cur'])
+    $(".rc-flat__more__btn").text(rus['rc-flat__more__btn"'])
+    $(".rc-flat__book__btn").text(rus['rc-flat__book__btn'])
+    $(".rc-flat_list .rc-flat:eq(0) .rc-flat__info").html(rus['rc-flat__info1'])
+    $(".rc-flat_list .rc-flat:eq(0) .rc-flat__more__info").html(rus['rc-flat__more__info1'])
+    $(".rc-flat_list .rc-flat:eq(1) .rc-flat__info").html(rus['rc-flat__info2'])
+    $(".rc-flat_list .rc-flat:eq(1) .rc-flat__more__info").html(rus['rc-flat__more__info2'])
   }
 }
-
-console.log(document.getElementsByClassName('rc-flat__book__btn')[0])
-
-
-// var myElement = document.getElementById('rc-bookings-widget-root');
-
-// if(window.addEventListener) {
-//    myElement.addEventListener('DOMSubtreeModified', contentChanged, false);
-// } else
-//    if(window.attachEvent) {
-//       myElement.attachEvent('DOMSubtreeModified', contentChanged);
-//    }
-
-// function contentChanged() {
-//     translateWidget(lang)
-//    // this function will run each time the content of the DIV changes
-
-// }
-document.getElementById('rc-bookings-widget-root').addEventListener('DOMSubtreeModified', function (event) {
-  document.getElementsByClassName('rc-flat__book__btn')[0].addEventListener('click', function(event) {
-    $('.rc-modal-title').text('booking')
-    event.preventDefault()
-  })
-})
